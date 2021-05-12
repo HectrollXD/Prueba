2816b6384dedf81a40926ac1b04800b9a0498b87
Hector <hector.rmtnez89@gmail.com>
2021-05-11 19:25:45 -0500

    Show all providers

--- AplicacionParaTienda/Providers/views.py
+++ AplicacionParaTienda/Providers/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 from django.shortcuts import redirect
+from django.shortcuts import render
 from project import NAME_OF_PROJECT
 from .forms import AddProviderForm
+from .models import Provider
 
 def providers_view( request ):
     form = AddProviderForm( request.POST or None )
+    providers = Provider.objects.all()
 
     if request.method == 'POST' and form.is_valid():
         form.save()
@@ -13,5 +15,6 @@ def providers_view( request ):
     return render( request, 'providers.html',{
         'titleOfPage': 'Providers',
         'nameOfApplication': NAME_OF_PROJECT,
-        'camp': form
+        'camp': form,
+        'providers': providers,
     } )
